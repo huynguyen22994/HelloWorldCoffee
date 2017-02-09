4a323935309a13a48ab34dd2e55c2d0c5b3abc0b
huy nguyen <huy.nguyen22994@gmail.com>
2017-02-10 00:05:56 +0700

    update source code - date: 10/2/2017

--- app/routes/coffees.js
+++ app/routes/coffees.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('HelloWorldCoffeeDB', ['coffees'])
+var db = mongojs('HelloWorldCoffeeDB', ['coffees']);
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
 
 router.get('/', function(req, res){
     db.coffees.find(function(err, docs){
@@ -12,6 +15,31 @@ router.get('/', function(req, res){
 router.get('/:id', function(req, res){
     var id = req.params.id;
      db.coffees.findOne({_id: mongojs.ObjectId(id)}, function(err, docs){
+        res.end(JSON.stringify(docs));
+    });
+});
+
+router.put('/', function(req, res){
+    db.coffees.findAndModify({query: {_id: mongojs.ObjectId(req.body._id)}, update: {$set:
+        {
+            name: req.body.name,
+            price: req.body.price,
+            img: req.body.img,
+            discribe: req.body.discribe
+        }
+    }, new: true}, function(err, docs){
+        res.end(JSON.stringify(docs));
+    });
+});
+
+router.post('/', function(req, res){
+    db.coffees.insert(req.body, function(err, docs){
+        res.end(JSON.stringify(docs));
+    });
+});
+
+router.delete('/:id', function(req, res){
+    db.coffees.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, docs){
         res.end(JSON.stringify(docs));
     });
 });
--- public/admin/js/controllers/coffeeAdmin.js
+++ public/admin/js/controllers/coffeeAdmin.js
@@ -1,18 +1,54 @@
 app.controller('CoffeeAdminCtrl', function($scope, CoffeeAdmin){
     //$scope.show = true;
-    //$scope.popup = true;
+    $scope.oneCoffee = '';
+    $scope.popup = true;
     refresh = function(){
         var coffees = CoffeeAdmin.query(function(){
-            console.log(coffees);
             $scope.coffees = coffees;
             //$scope.show = false;
         });
     }
     refresh()
-    //$scope.detail = function(coffeeid){
-    //    $scope.oneCoffee = Coffee.get({id: coffeeid});
-    //    $scope.popup = false;
-    //};
+    $scope.detailAdmin = function(coffeeid){
+        $scope.oneCoffee = CoffeeAdmin.get({id: coffeeid}, function(){
+
+        });
+        //$scope.popup = false;
+    };
+
+    $scope.updateCoffee = function(coffeeid){
+        $scope.coffee = CoffeeAdmin.get({id: coffeeid}, function(){
+            $scope.coffee.name = $scope.oneCoffee.name;
+            $scope.coffee.price = $scope.oneCoffee.price;
+            $scope.coffee.img = $scope.oneCoffee.img;
+            $scope.coffee.discribe = $scope.oneCoffee.discribe;       
+            $scope.coffee.$update(function(){
+                refresh();
+            });
+        });
+    };
+
+    $scope.addCoffee = function(){
+        console.log($scope.newCoffee.img);
+        $scope.addCoffee = new CoffeeAdmin();
+        $scope.addCoffee.name = $scope.newCoffee.name;
+        $scope.addCoffee.price = $scope.newCoffee.price;
+        $scope.addCoffee.img = "/img/coffees/capuchino.jpg";
+        $scope.addCoffee.discribe = $scope.newCoffee.discribe;
+        CoffeeAdmin.save($scope.addCoffee, function(){
+            alert('add success');
+            refresh();
+        });
+    };
+
+    $scope.deleteCoffee = function(coffeeid){
+        $scope.coffee = CoffeeAdmin.get({id: coffeeid}, function(){
+            $scope.coffee.id = coffeeid;
+            $scope.coffee.$delete(function(){
+                refresh();
+            });
+        });
+    };
 
     //$scope.clear = function(){
     //    $scope.oneCoffee = null;
